0892967e54e70b35d72195243d08e9f131579abb
Bayu Rizki Rahmadi <bayu.r.rahmadi@gmail.com>
2016-10-21 10:16:26 +0700

    Add function to count total price

--- app/code/community/Doku/Onecheckout/controllers/IndexController.php
+++ app/code/community/Doku/Onecheckout/controllers/IndexController.php
@@ -34,33 +34,116 @@ class Doku_Onecheckout_IndexController extends Mage_Core_Controller_Front_Action
 	 */
 
 	public function paymentAction() {
+
+    // This Additional Code Written By Bayu
+    // Code for send data to seller dashboard
+    $session = $this->_getCheckout();
+    $order = Mage::getModel('sales/order');
+
+    $items = $order->getAllItems();
+    $item_details = array();
+    foreach ($items as $each) {
+        $item = array(
+            'product_name'  => $each->getName(),
+            'product_sku'   => $each->getSku(),
+            'product_price' => $this->is_string($each->getPrice()),
+            'quantity'      => $this->is_string($each->getQtyToInvoice()),
+        );
+
+        if ($item['quantity'] == 0) {
+            continue;
+        }
+
+        $item_details[] = $item;
+    }
+    unset($each);
+
+    if ($discount_amount != 0) {
+        $couponItem = array(
+            'product_name'  => 'DISCOUNT',
+            'product_sku'   => 'DISCOUNT',
+            'product_price' => $discount_amount,
+            'quantity'      => 1,
+        );
+        $item_details[] = $couponItem;
+    }
+
+    if ($shipping_amount > 0) {
+        $shipping_item = array(
+            'product_name'  => 'Shipping Cost',
+            'product_sku'   => 'SHIPPING',
+            'product_price' => $shipping_amount,
+            'quantity'      => 1,
+        );
+        $item_details[] = $shipping_item;
+    }
+
+    if ($shipping_tax_amount > 0) {
+        $shipping_tax_item = array(
+            'product_name'  => 'Shipping Tax',
+            'product_sku'   => 'SHIPPING_TAX',
+            'product_price' => $shipping_tax_amount,
+            'quantity'      => 1,
+        );
+        $item_details[] = $shipping_tax_item;
+    }
+
+    if ($tax_amount > 0) {
+        $tax_item = array(
+            'product_name'  => 'Tax',
+            'product_sku'   => 'TAX',
+            'product_price' => $tax_amount,
+            'quantity'      => 1,
+        );
+        $item_details[] = $tax_item;
+    }
+
+    $current_currency = Mage::app()->getStore()->getCurrentCurrencyCode();
+    if ($current_currency != 'IDR') {
+        $conversion_func = function ($non_idr_price) {
+            return $non_idr_price * Mage::helper('kredivopayment')->_getConversionRate();
+        };
+        foreach ($item_details as &$item) {
+            $item['product_price'] = intval(round(call_user_func($conversion_func, $item['product_price'])));
+        }
+        unset($item);
+    } else {
+        foreach ($item_details as &$each) {
+            $each['product_price'] = (int) $each['product_price'];
+        }
+        unset($each);
+    }
+    $totalPrice = 0;
+    foreach ($item_details as $item) {
+        $totalPrice += $item['product_price'] * $item['quantity'];
+    }
+
+
+    $site_name = "http://vendor.tinkerlust.com";
+
+    $name = $order->getBillingAddress()->getName();
+    $email = $order_billing_address->getEmail();
+    $order_no = $order->loadByIncrementId($session->getLastRealOrderId());
+    $total = $this->is_string($totalPrice);
+    $transfer_no = 212;
+    $transfer_bank = "DOKU";
+
+    $name = urlencode($name);
+    $email = urlencode($email);
+    $order_no = urlencode($order_no);
+    $total = urlencode($total);
+    $transfer_no = urlencode($transfer_no);
+    $transfer_bank = urlencode($transfer_bank);
+
+    $url_go = "$site_name/api_paymentconfirm.php?payment_sd=yes&name=$name&email=$email&order_no=$order_no&total=$total&transfer_no=$transfer_no&transfer_bank=$transfer_bank";
+    $content = Mage::helper('kredivopayment')->bacaHTML($url_go);
+    //echo $content;
+    // Code for send data to seller dashboard ends
+
         try {
             $session = $this->_getCheckout();
             $order = Mage::getModel('sales/order');
             $order->loadByIncrementId($session->getLastRealOrderId());
-
-            // This Additional Code Written By Bayu
-            // Code for send data to seller dashboard
-            $site_name = "http://vendor.tinkerlust.com";
-
-            $name = $order->getBillingAddress()->getName();
-            $email = $order_billing_address->getEmail();
-            $order_no = $order->loadByIncrementId($session->getLastRealOrderId());
-            $total = $this->is_string($totalPrice);
-            $transfer_no = 212;
-            $transfer_bank = "DOKU";
-
-            $name = urlencode($name);
-            $email = urlencode($email);
-            $order_no = urlencode($order_no);
-            $total = urlencode($total);
-            $transfer_no = urlencode($transfer_no);
-            $transfer_bank = urlencode($transfer_bank);
-
-            $url_go = "$site_name/api_paymentconfirm.php?payment_sd=yes&name=$name&email=$email&order_no=$order_no&total=$total&transfer_no=$transfer_no&transfer_bank=$transfer_bank";
-            $content = Mage::helper('kredivopayment')->bacaHTML($url_go);
-            //echo $content;
-            // Code for send data to seller dashboard ends
 
             if (!$order->getId()) {
                 Mage::throwException('No order for processing found');
